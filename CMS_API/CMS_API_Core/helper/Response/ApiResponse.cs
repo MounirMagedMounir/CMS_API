ae0a8b30a331afd716e14f096824636b7dfcbb1c
MounirMagedMounir <115494921+MounirMagedMounir@users.noreply.github.com>
2024-12-05 14:47:51 +0200

    Change the response Structure

---
+++ CMS_API/CMS_API_Core/helper/Response/ApiResponse.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace CMS_API_Core.helper.Response
+{
+    public class ApiResponse<T>(IEnumerable<T>? data, int status, IEnumerable<string> message)
+    {
+        public int Status { get; set; } = status;
+        public IEnumerable<string> Message { get; set; } = message;
+        public IEnumerable<T>? Data { get; set; } = data;
+
+    }
+
+}
---
+++ CMS_API/CMS_API_Core/helper/Response/MetaData.cs
@@ -0,0 +1,30 @@
+﻿namespace CMS_API_Core.helper.Response
+{
+    public class MetaData<T>
+    {
+        public T Filters { get; set; }
+        public string SortBy { get; set; }
+        public string SortOrder { get; set; }
+        public int page { get; set; }
+        public int PerPage { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalItems { get; set; }
+        public MetaData()
+        {
+
+        }
+
+        public MetaData(int _page, int _PerPage, int _TotalItems, T _Filters, string _SortBy = "name", string _SortOrder = "ase")
+        {
+            SortBy = _SortBy;
+            SortOrder = _SortOrder;
+            page = _page;
+            PerPage = _PerPage;
+            TotalPages = (int)Math.Ceiling((double)_TotalItems / _PerPage);
+            TotalItems = _TotalItems;
+            Filters = _Filters;
+
+        }
+
+    }
+}
